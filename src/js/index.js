e109b64101704f7b9302b741f42bf7875df40af9
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-09-16 00:53:03 +0300

    Futures development

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,40 @@ $(document).ready(function () {
     $('.tabs-content .item').eq(current).addClass('display').siblings().removeClass('display');
   });
 
+//Prallax
+  $(window).scroll(function(){
+    var pralaxedEl = $('.prallax'),
+        heightTop = pralaxedEl.offset().top,
+        windowHeight = $(window).height(),
+        windowScrool = $(this).scrollTop();
+    if ((heightTop-windowHeight) < windowScrool){
+      var scrolled = $(window).scrollTop();
+      pralaxedEl.css('background-position-y',-(scrolled*0.2)+'px');
+    }
+  });
+
+//Slider
+  var item = $('.testimonial-slider li'),
+      itemsCount = item.length,
+      itemsWidth = $('.slider').width() / 3;
+
+  item.css('width', itemsWidth + 'px');
+
+  $('.testimonial-slider').css('width', itemsCount * itemsWidth +'px');
+
+  $('#prev').on('click', function () {
+    var last = $('.testimonial-slider li').last().css({opacity: '0', width: '0px'});
+    last.prependTo('.testimonial-slider');
+    last.animate({opacity: '1', width: itemsWidth + 'px'});
+  });
+
+  $('#next').on('click', function () {
+    var first = $('.testimonial-slider li').first();
+    first.animate({opacity: '0', width: '0px'}, function() {
+      first.appendTo('.testimonial-slider').css({opacity: '1', width: itemsWidth + 'px'});
+    });
+  });
+
 });
 
 
