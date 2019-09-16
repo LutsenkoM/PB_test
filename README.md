29b0b2c84660de03137c6f79e89c8fe69b26abcc
Max Lutsenko <lutsenko.maxim@gmail.com>
2019-09-17 02:01:16 +0300

    Responsive, mobile menu, write README

---
+++ README.md
@@ -0,0 +1,5 @@
+For project setup run following commands in root folder:
+
+- npm install;
+- gulp;
+- open index.html in browser;
--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,78 @@
 $(document).ready(function () {
+    
+    // Mobile menu
+    var menuOpen = false,
+        mainNav = $('.main-nav');
+    $('#menu-btn').click(function () {
+       if(menuOpen) {
+           mainNav.slideUp(500);
+       } else {
+           mainNav.slideDown(500);
+       }
+       menuOpen = !menuOpen;
+    });
 
-// Tabs
-  $('.tabs-nav li').click(function () {
-    var current = $(this).index();
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.tabs-content .item').eq(current).addClass('display').siblings().removeClass('display');
-  });
+    $('.submenu-isset').click(function (e) {
+        e.preventDefault();
+       $(this).children('.sub-menu-hidden').toggleClass('visible');
+       $(this).siblings().children('.sub-menu-hidden').removeClass('visible');
+    });
 
-//Prallax
-  $(window).scroll(function(){
-    var pralaxedEl = $('.prallax'),
-        heightTop = pralaxedEl.offset().top,
-        windowHeight = $(window).height(),
-        windowScrool = $(this).scrollTop();
+    // Tabs
+    $('.tabs-nav li').click(function () {
+        var current = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.tabs-content .item').eq(current).addClass('display').siblings().removeClass('display');
+    });
+
+    //Prallax
+    $(window).scroll(function(){
+        var pralaxedEl = $('.prallax'),
+            heightTop = pralaxedEl.offset().top,
+            windowHeight = $(window).height(),
+            windowScrool = $(this).scrollTop();
     if ((heightTop-windowHeight) < windowScrool){
-      var scrolled = $(window).scrollTop();
-      pralaxedEl.css('background-position-y',-(scrolled*0.2)+'px');
+        var scrolled = $(window).scrollTop();
+        pralaxedEl.css('background-position-y',-(scrolled*0.2)+'px');
     }
-  });
+    });
 
 //Slider
-  var item = $('.testimonial-slider li'),
-      itemsCount = item.length,
-      itemsWidth = $('.slider').width() / 3;
+    var item = $('.testimonial-slider li'),
+        itemsCount = item.length,
+        slider = $('.slider'),
+        itemsWidth;
 
-  item.css('width', itemsWidth + 'px');
+    if ($(window).width() > 991) {
+      itemsWidth = slider.width() / 3;
+    } else if ($(window).width() < 991) {
+      itemsWidth = slider.width() / 2;
+    } else if ($(window).width() < 768) {
+      itemsWidth = slider.width()
+    }
 
-  $('.testimonial-slider').css('width', itemsCount * itemsWidth +'px');
+    item.css('width', itemsWidth + 'px');
 
-  $('#prev').on('click', function () {
+    $('.testimonial-slider').css('width', itemsCount * itemsWidth +'px');
+
+    $('#prev').on('click', function () {
     var last = $('.testimonial-slider li').last().css({opacity: '0', width: '0px'});
     last.prependTo('.testimonial-slider');
     last.animate({opacity: '1', width: itemsWidth + 'px'});
-  });
+    });
 
-  $('#next').on('click', function () {
+    $('#next').on('click', function () {
     var first = $('.testimonial-slider li').first();
     first.animate({opacity: '0', width: '0px'}, function() {
       first.appendTo('.testimonial-slider').css({opacity: '1', width: itemsWidth + 'px'});
     });
-  });
+    });
+
+    // Submit form
+    $("form").submit(function(event) {
+    console.log( $(this).serializeArray() );
+    event.preventDefault();
+    });
 
 });
 
